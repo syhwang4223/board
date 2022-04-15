b4b7f7b6e9c39699baa944917c332b0ab1e52d19
syhwang4223 <syhwang4223@gmail.com>
2022-04-15 19:38:43 +0900

    [Add] LoginMemberArgumentResolver 를 WebConfig 에 등록

--- src/main/java/com/syhwang/board/WebConfig.java
+++ src/main/java/com/syhwang/board/WebConfig.java
@@ -1,11 +1,19 @@
 package com.syhwang.board;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new LoginMemberArgumentResolver());
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
